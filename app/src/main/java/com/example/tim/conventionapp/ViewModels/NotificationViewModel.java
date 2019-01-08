6ecc666f5602e12833bdb06abf8a7b538b44cd6a
Tim Wyrick <twyrick94@gmail.com>
2019-01-08 00:22:22 -0600

    Updated NotificationViewModel

--- app/src/main/java/com/example/tim/conventionapp/ViewModels/NotificationViewModel.java
+++ app/src/main/java/com/example/tim/conventionapp/ViewModels/NotificationViewModel.java
@@ -3,4 +3,26 @@ package com.example.tim.conventionapp.ViewModels;
 import android.arch.lifecycle.ViewModel;
 
 public class NotificationViewModel extends ViewModel {
+    private String Title;
+    private String Message;
+
+    public String GetTitle()
+    {
+        return this.Title;
+    }
+
+    public void SetTitle(String title)
+    {
+        this.Title = title;
+    }
+    
+    public String GetMessage()
+    {
+        return this.Message;
+    }
+
+    public void SetMessage(String message)
+    {
+        this.Message = message;
+    }
 }
