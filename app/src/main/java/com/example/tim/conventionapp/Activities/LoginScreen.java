b642b16a69fe89df6ecada85f83860c5c3c36c06
Tim Wyrick <twyrick94@gmail.com>
2019-01-08 21:59:34 -0600

    Set up login screen and data binding

---
+++ app/src/main/java/com/example/tim/conventionapp/Activities/LoginScreen.java
@@ -0,0 +1,15 @@
+package com.example.tim.conventionapp.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.tim.conventionapp.R;
+
+public class LoginScreen  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
